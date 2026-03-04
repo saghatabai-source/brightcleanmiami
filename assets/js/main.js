fd451219a9089e837c19bc918db5c912f2431eec
saghatabai-source <s.aghatabai@gmail.com>
2026-03-04 05:50:23 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,29 @@
+// MOBILE MENU
 
+document.addEventListener("DOMContentLoaded", function(){
+
+const menuBtn = document.querySelector(".menu-toggle");
+const nav = document.querySelector(".main-nav");
+
+if(menuBtn){
+
+menuBtn.addEventListener("click", function(){
+
+if(nav.style.display === "flex"){
+nav.style.display = "none";
+}else{
+nav.style.display = "flex";
+nav.style.flexDirection = "column";
+nav.style.position = "absolute";
+nav.style.top = "70px";
+nav.style.right = "20px";
+nav.style.background = "#fff";
+nav.style.padding = "20px";
+nav.style.boxShadow = "0 10px 30px rgba(0,0,0,0.1)";
+}
+
+});
+
+}
+
+});
