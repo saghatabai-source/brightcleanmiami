f2e94bce984d33cf52a893fc2e4554d359bd77b0
saghatabai-source <s.aghatabai@gmail.com>
2026-03-04 19:52:43 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,24 +4,16 @@ const menuBtn = document.querySelector(".menu-toggle");
 const nav = document.querySelector(".main-nav");
 
 if(menuBtn){
-
 menuBtn.addEventListener("click", function(){
 
 if(nav.style.display === "flex"){
 nav.style.display = "none";
-}else{
+}
+else{
 nav.style.display = "flex";
-nav.style.flexDirection = "column";
-nav.style.position = "absolute";
-nav.style.top = "70px";
-nav.style.right = "20px";
-nav.style.background = "#fff";
-nav.style.padding = "20px";
-nav.style.boxShadow = "0 10px 30px rgba(0,0,0,0.1)";
 }
 
 });
-
 }
 
 });
